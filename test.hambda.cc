1c70e014b82b67165240f571b33c17a60c4dc769
Aaron McDaid <aaronmcdaid@gmail.com>
2017-11-12 22:20:55 +0000

    Define 'cout<<' and '==' for 'char_pack'

--- test.hambda.cc
+++ test.hambda.cc
@@ -4,11 +4,24 @@
 
 using hambda::operator"" _charpack;
 
-template<char ...c>
-std::ostream & operator<<(std::ostream &o, utils::char_pack<c...> str)
-{
-    o << str.c_str0();
-    return o;
+namespace utils { // 'utils' namespace, in order to use ADL
+
+    template<char ...c>
+    std::ostream & operator<<(std::ostream &o, utils::char_pack<c...> str)
+    {
+        o << str.c_str0();
+        return o;
+    }
+
+    template<char ...c>
+    static constexpr auto
+    operator==(std::string const & l, utils::char_pack<c...> r)
+    { return l == r.c_str0(); }
+
+    template<char ...c>
+    static constexpr auto
+    operator==(utils::char_pack<c...> l, std::string const & r)
+    { return r == l.c_str0(); }
 }
 
 using namespace hambda;
@@ -38,6 +51,11 @@ int main() {
             , 87
             ) ^ []()
             { return simplify(parse_ast("((((((id id) id) id) id) -) ((id +) ((((id id) (id id)) +) (id 90) 9) 0) (+ 5 7))"_charpack)); };
+
+    TEST_ME ( "string literals"
+            , std::string("helloworld")
+            ) ^ []()
+            { return simplify(parse_ast("'helloworld'"_charpack)); };
 
     std::cout << simplify(parse_ast( "(+ (+ (+ 90 9) 0) (+ 5 7))"_charpack)) << '\n';
     std::cout << simplify(parse_ast( "'''e''''o'"_charpack)) << '\n';
